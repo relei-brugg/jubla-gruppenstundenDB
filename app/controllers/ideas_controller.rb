d3d5492b82deb948b52a76934b2e4171ef1a6705
Ivo Nussbaumer <ivo.nussbaumer@gmail.com>
2013-12-28 12:03:05 +0100

    multistep form

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -9,22 +9,52 @@ class IdeasController < ApplicationController
   end
 
   def new
-    @idea = Idea.new
+    session[:idea_params] ||= {}
+    @idea = Idea.new(session[:idea_params])
+    @idea.current_step = session[:idea_step]
   end
 
   def create
-    @idea = Idea.new(idea_params)
-    @idea.location_category_ids = params[:idea][:location_category_ids]
-    @idea.age_category_ids = params[:idea][:age_category_ids]
-    @idea.activity_category_ids = params[:idea][:activity_category_ids]
-    @idea.method_category_ids = params[:idea][:method_category_ids]
-    @idea.season_category_ids = params[:idea][:season_category_ids]
-    @idea.group_size_category_ids = params[:idea][:group_size_category_ids]
-    if @idea.save
-      redirect_to @idea
-    else
-      render 'new'
+    # merge session with post params
+    session[:idea_params].deep_merge!(idea_params) if idea_params
+
+    @idea = Idea.new(session[:idea_params])
+    @idea.current_step = session[:idea_step]
+
+    if @idea.valid?
+      if params[:back_button]
+        @idea.previous_step
+      elsif @idea.last_step?
+        @idea.save if @idea.all_valid?
+      else
+        @idea.next_step
+      end
+      session[:idea_step] = @idea.current_step
     end
+
+    if @idea.new_record?
+      render 'new'
+    else
+      session[:idea_step] = session[:idea_params] = nil
+      flash[:success] = 'Idea saved!'
+      redirect_to @idea
+    end
+
+
+
+
+    #@idea = Idea.new(idea_params)
+    #@idea.location_category_ids = params[:idea][:location_category_ids]
+    #@idea.age_category_ids = params[:idea][:age_category_ids]
+   # @idea.activity_category_ids = params[:idea][:activity_category_ids]
+   # @idea.method_category_ids = params[:idea][:method_category_ids]
+   # @idea.season_category_ids = params[:idea][:season_category_ids]
+   # @idea.group_size_category_ids = params[:idea][:group_size_category_ids]
+    #if @idea.save
+    #  redirect_to @idea
+   # else
+   #   render 'new'
+   # end
   end
 
   def edit
@@ -33,12 +63,12 @@ class IdeasController < ApplicationController
 
   def update
     @idea = Idea.find(params[:id])
-    @idea.location_category_ids = params[:idea][:location_category_ids]
-    @idea.age_category_ids = params[:idea][:age_category_ids]
-    @idea.activity_category_ids = params[:idea][:activity_category_ids]
-    @idea.method_category_ids = params[:idea][:method_category_ids]
-    @idea.season_category_ids = params[:idea][:season_category_ids]
-    @idea.group_size_category_ids = params[:idea][:group_size_category_ids]
+    #@idea.location_category_ids = params[:idea][:location_category_ids]
+    #@idea.age_category_ids = params[:idea][:age_category_ids]
+    #@idea.activity_category_ids = params[:idea][:activity_category_ids]
+    #@idea.method_category_ids = params[:idea][:method_category_ids]
+    #@idea.season_category_ids = params[:idea][:season_category_ids]
+    #@idea.group_size_category_ids = params[:idea][:group_size_category_ids]
     if @idea.update(idea_params)
       redirect_to @idea
     else
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,12 +1,47 @@
 class Idea < ActiveRecord::Base
 
   has_many :comments, dependent: :destroy
-  has_and_belongs_to_many :activity_categories
-  has_and_belongs_to_many :age_categories
-  has_and_belongs_to_many :group_size_categories
-  has_and_belongs_to_many :location_categories
-  has_and_belongs_to_many :method_categories
-  has_and_belongs_to_many :season_categories
+  #has_and_belongs_to_many :activity_categories
+  #has_and_belongs_to_many :age_categories
+  #has_and_belongs_to_many :group_size_categories
+  #has_and_belongs_to_many :location_categories
+  #has_and_belongs_to_many :method_categories
+  #has_and_belongs_to_many :season_categories
 
-  validates :title, presence: true, length: { minimum: 5 }
+  validates :title, presence: true, length: { minimum: 5 }, if: :first_step?
+
+
+  # Multistep form code
+  attr_writer :current_step
+
+  def current_step
+    @current_step || steps.first
+  end
+
+  def steps
+    %w[1 2 3]
+  end
+
+  def next_step
+    self.current_step = steps[steps.index(current_step)+1]
+  end
+
+  def previous_step
+    self.current_step = steps[steps.index(current_step)-1]
+  end
+
+  def first_step?
+    current_step == steps.first
+  end
+
+  def last_step?
+    current_step == steps.last
+  end
+
+  def all_valid?
+    steps.all? do |step|
+      self.current_step = step
+      valid?
+    end
+  end
 end
