d1d1bf1a6b15f0839357564033a0a877393728a1
Ivo Nussbaumer <ivo.nussbaumer@gmail.com>
2014-01-10 19:32:43 +0100

    db fix

--- app/helpers/ideas_helper.rb
+++ app/helpers/ideas_helper.rb
@@ -42,7 +42,9 @@ module IdeasHelper
       end
 
       if @filter.content != ''
-        @ideas = @ideas.where("(LOWER(start) LIKE ? OR LOWER(main_part) LIKE ? OR LOWER(end) LIKE ?)", ls(@filter.content), ls(@filter.content), ls(@filter.content))
+        at = Idea.arel_table
+        @ideas = @ideas.where(at[:start].matches("%#{ls(@filter.content)}%").or(at[:main_part].matches("%#{ls(@filter.content)}%")).or(at[:end].matches("%#{ls(@filter.content)}%")))
+        #@ideas = @ideas.where("LOWER(start) LIKE ? OR LOWER(main_part) LIKE ? OR LOWER(end) LIKE ?", ls(@filter.content), ls(@filter.content), ls(@filter.content))
       end
 
       # associations filter
